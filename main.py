dd83f6a49dd5996779aa2bc026cd79a182778de0
Linda Nguyen <lngu0107@student.monash.edu>
2025-01-18 18:30:12 +1100

    adopted more organised format where use of actions needed for the user is centralised at main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# whaaat that's crazy! hahahaha
+# --- EXPORTS ---
+from actions import initialise_database, view_database
+
+# the big boss function
+if __name__ == "__main__":
+    initialise_database()
+    view_database()
